3726663df9ab91c467144ec28a8f0b1750356f6c
Adewale Adetoro <adetorodev19@gmail.com>
2023-08-09 01:49:05 +0100

    type conversion

---
+++ typeConversion.js
@@ -0,0 +1,37 @@
+// String
+let value =  true; // boolean
+// console.log(typeof value);
+valueChange = String(value); // string
+// console.log(valueChange)
+// console.log(typeof valueChange)
+
+let x = 234; // number
+let xString = String(x) // string
+// console.log(typeof xString)
+
+// Number 
+// console.log("6" + "2") // string concatenation
+
+let str = "234";
+let num =  Number(str)
+// console.log(typeof num)
+
+let name =  Number("John")
+// console.log(name)
+
+// console.log(Number(undefined))
+// console.log(Number(null))
+// console.log(Number(true))
+// console.log(Number(false))
+
+// console.log(1 + "3")
+// console.log(2 + 3 + "5") // 2=5 => 5 + 5 => 55
+
+// Boolean
+// console.log(Boolean(1))
+// console.log(Boolean(0))
+console.log(Boolean("Yes"))
+console.log(Boolean("No"))
+
+console.log(Boolean("hello"))
+console.log(Boolean(""))
