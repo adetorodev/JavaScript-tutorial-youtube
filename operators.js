aadb49f49c799b3a77ae85257da0ca16e351a109
Adewale Adetoro <adetorodev19@gmail.com>
2023-08-13 17:26:30 -0700

    operators

---
+++ operators.js
@@ -0,0 +1,85 @@
+// + , -, * , /, %
+
+// let x  =  'my' + 'name'  // String Concatenation
+// console.log(x)
+// console.log('1' + 3) // 13
+// console.log(3 + '1') // 31
+// console.log(2+2+'2')
+
+// console.log(2 - '1')
+// console.log('6' / 2)
+
+// Number('2')
+
+// let x = 6
+// console.log(+x)
+
+// let y = '3'
+// let x = +y
+// console.log(typeof x)
+
+// console.log(+true)
+// console.log(+"")
+
+// + conversion 
+
+// let user = '5';
+// let x = '5'
+// console.log(+user + +x)
+// console.log(Number(user) + Number(x))
+
+// equal to (=)
+
+// let x = 2
+// let y = 'Name'
+
+// let a, b, c; // chain assignment
+
+//  a = b = c = 2 + 2 // 4
+//  console.log(b)
+
+
+// let a = 4
+// let b = 3
+
+// let c = 1 - (a = b + 5) // a = 3
+// console.log(c)
+// console.log(a)
+
+// % remainder => modulus
+
+// console.log(11 % 3)
+
+
+// ** Exponentitation
+//  console.log(2 ** 3) // 2 x 2 x 2
+
+//  console.log((3**9))
+
+//  let counter = 2
+//  counter++  // counter + 1
+//  console.log(counter)
+
+//  counter--
+//  console.log(counter)
+
+//  let a = ++counter
+//  console.log(a)
+
+//  let x = counter++
+//  console.log(x)
+
+//  let z = --counter
+//  let s =  --counter
+//  console.log(z)
+//  console.log(s)
+
+//  let counter = 2;
+//  console.log(2 * ++counter) // 6
+//  console.log(2 * counter++)
+//  console.log(2 * counter--) // no decrement
+// console.log(2 * --counter) // counter = 0
+
+
+let c = ( 3 + 2,4 + 2);
+console.log(c)
