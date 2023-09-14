bda8b3a99c6adc94881b6e7ce810b87f4d322ca8
Ezekiel Adetoro <adetorodev19@gmail.com>
2023-09-14 08:40:18 -0500

    comparison

---
+++ comparisions.js
@@ -0,0 +1,27 @@
+// console.log(2<1)
+// console.log(2 == 1)
+
+// let result = 5 > 4
+// console.log( result )
+// console.log("first result", "Z" > "A")
+// console.log("Second Result", "Bee" > "Be")
+// console.log("Glow" > "Glee")
+// console.log("2" > 1) // 2 > 2
+// console.log(1 == "01") // 1 == 1
+
+// console.log(true == 1)
+// console.log(false == 0)
+
+// let a  = 0;
+// console.log(Boolean(a))
+
+// let b  = "0";
+// console.log(Boolean(b))
+
+// console.log(a === b)
+// ==
+/// ===
+
+console.log(0 === false)
+console.log(null === undefined)
+console.log(null == undefined)
