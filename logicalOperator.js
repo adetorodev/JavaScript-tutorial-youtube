ee3119ee2b93d4f77b41f9ac937c632d1cd0d8b7
Ezekiel Adetoro <adetorodev19@gmail.com>
2023-09-14 17:24:41 -0500

    logical Operator

---
+++ logicalOperator.js
@@ -0,0 +1,38 @@
+// || OR
+// && AND
+// ! NOT
+
+// || OR
+// console.log(true || true)
+// console.log(true || false)
+// console.log(false || true)
+// console.log(false || false)
+
+// let businessHour = 12;
+// if(businessHour < 8 || businessHour > 17) {
+//     console.log("We are close for the day.")
+// } else{
+//     console.log("We are open for business!")
+// }
+
+// && AND
+// console.log(true && true)
+// console.log(true && false)
+// console.log(false && true)
+// console.log(false && false)
+
+// let businessHour = 7;
+// if(businessHour > 8 && businessHour < 17) {
+//     console.log("We are open for business.")
+// } else{
+//     console.log("We are close for the day!")
+// }
+
+// console.log((true && true) || (false && true) )
+
+// ! NOT
+
+console.log(!true)
+
+console.log(!!"")
+console.log(!!null)
